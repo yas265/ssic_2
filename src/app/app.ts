d2ec6bb376627e55e690896f0e83c863e9966885
Yashvant Singh Yadav <yashawantyadav265@gmail.com>
2025-06-01 14:44:28 +0530

    inititated angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Dashboard } from '../dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
