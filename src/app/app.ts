7dde9fc32d09275f423eb0c3c88dfa086f6deec7
Yashvant Singh Yadav <yashawantyadav265@gmail.com>
2025-06-02 17:32:47 +0530

    First Page UI Completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dashboard } from '../dashboard/dashboard';
+import { Faculty } from '../faculty/faculty';
+import { CommonModule } from '@angular/common';
+import { Nav } from '../nav/nav';
+import { Banner } from '../banner/banner';
+import { Footer } from '../footer/footer';
+import { Galary } from '../galary/galary';
+import { Announcement } from '../announcement/announcement';
+import { ManagerMessage } from '../manager-message/manager-message';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard],
+  imports: [RouterOutlet, Dashboard, Faculty, Nav, Banner,Footer, Galary, Announcement, ManagerMessage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/galary/galary.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galary',
+  imports: [],
+  templateUrl: './galary.html',
+  styleUrl: './galary.css'
+})
+export class Galary {
+
+}
